baa1794a74a3892e4b9f0afda6c09ff7c73be4a9
Hung Nguyen <hung@fastsoft.com.au>
2015-04-01 10:41:52 +0800

    Spec Info model

---
+++ app/src/main/java/com/onesys/onemarket/model/SpecificationInfo.java
@@ -0,0 +1,61 @@
+package com.onesys.onemarket.model;
+
+/**
+ * Created by Hung on 1/04/2015.
+ */
+
+import java.io.Serializable;
+
+public class SpecificationInfo
+        implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    private String audio_standard;
+    private String audio_type;
+    private String battery;
+    private String battery_call;
+    private String battery_waiting;
+    private String browser;
+    private String camera;
+    private String camera_record_video;
+    private String camera_sub;
+    private String chipset;
+    private String chipset_detail;
+    private String cpu;
+    private String data_3g_speed;
+    private String data_bluetooth;
+    private String data_edge;
+    private String data_gprs;
+    private String data_infrared;
+    private String data_nfc;
+    private String data_usb;
+    private String data_wlan;
+    private String game;
+    private String gps;
+    private String id;
+    private String info_2g;
+    private String info_3g;
+    private String info_date_present;
+    private String info_date_sale;
+    private String java_more_info;
+    private String java_support;
+    private String language;
+    private String memory_called;
+    private String memory_contact;
+    private String memory_hdd;
+    private String memory_ram;
+    private String memory_slot;
+    private String more_info;
+    private String notice;
+    private String os_detail;
+    private String radio;
+    private String size;
+    private String sms;
+    private String sreen_more_info;
+    private String sreen_size;
+    private String sreen_size_ppi;
+    private String sreen_size_width_height;
+    private String sreen_type;
+    private String weight;
+
+}
